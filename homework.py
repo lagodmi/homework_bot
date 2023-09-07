3d825723ee6f1e3555d38484f2ba2fad4cf4ec03
Dmitrii <lagodnov@gmail.com>
2023-09-08 00:28:36 +0300

    почти готов

--- homework.py
+++ homework.py
@@ -1,19 +1,24 @@
+import logging
 import os
 import requests
 import schedule
 import time
-from telegram import ReplyKeyboardMarkup, Bot
-from telegram.ext import CommandHandler, Updater
-
+from telegram import Bot
 from dotenv import load_dotenv
 
-import errors
 
 load_dotenv()
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename='main.log',
+    filemode='w',
+    format='%(asctime)s, %(levelname)s, %(message)s'
+)
+
 
 PRACTICUM_TOKEN = 'y0_AgAAAAAyJ55PAAYckQAAAADr4_HlSWx5X1b-T3KZweaLtzSJtqzPi30'
-TELEGRAM_TOKEN = os.getenv('TOKEN')
+TELEGRAM_TOKEN = os.getenv('Token')
 TELEGRAM_CHAT_ID = 5766924512
 
 RETRY_PERIOD = 600
@@ -29,87 +34,81 @@ HOMEWORK_VERDICTS = {
 
 
 def check_tokens():
-    """Проверяет наличие токенов."""
+    """Проверяет доступность переменных окружения."""
     if not PRACTICUM_TOKEN:
-        raise errors.PracticumTokenError(
+        logging.critical(
             'Ошибка: токен для Яндекс Практикума не найден.'
         )
     if not TELEGRAM_TOKEN:
-        raise errors.TelegramTokenError(
+        logging.critical(
             'Ошибка: токен для Telegram не найден.'
         )
 
 
-def send_message(bot, message):
-    """Отправляет в telegram сообщение."""
-    bot.sendMessage(TELEGRAM_CHAT_ID, message)
-
-
 def get_api_answer(timestamp):
-    """Получаем ответ от Яндекс Практикума."""
-    return requests.get(
-        ENDPOINT,
-        headers={'Authorization': PRACTICUM_TOKEN},
-        params={'from_date': timestamp}
-    )
+    """Делает запрос к эндпоинту."""
+    try:
+        response = requests.get(
+            ENDPOINT,
+            headers=HEADERS,
+            params={'from_date': timestamp}
+        )
+    except Exception:
+        logging.error('Ошибка при запросе к API.')
+    return response.json()
 
 
 def check_response(response):
-    """Проверка на обновление статуса."""
-    result = False
-    try:
-        # Пытаемся открыть файл.
-        with open('answer.txt', 'r+', encoding='utf8') as file:
-            # Читаем последний статус.
-            status = file.readlines()[-1].strip()
-            # Если текуший статус не совпадает и он не False.
-            if status != response and response:
-                # Записываем тикущий статус и возвращаем True.
-                file.write(response + '\n')
-                result = True
-    except FileNotFoundError:
-        # Если файл не найден будет создан.
-        with open('answer.txt', 'w', encoding='utf8') as file:
-            # Записываем тикущий статус.
-            file.write(response + '\n')
-            # Если текуший статус True возвращаем True.
-            if response:
-                result = True
-    return result
+    """Проверяет ответ API на соответствие документации."""
+    if not response['homeworks']:
+        logging.debug('API не соответствует.')
 
 
 def parse_status(homework):
     """Извлекаем статус из ответа с Практикума."""
-    homework_name = homework['homeworks'][0]['homework_name'][9:-4]
-    verdict = homework['homeworks'][0]['status']
-    # Проверка изменился ли статус.
-    if check_response():
-        return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+    try:
+        homework_name = homework['homeworks'][0]['homework_name'][9:-4]
+        status = homework['homeworks'][0]['status']
+    except Exception:
+        logging.error('Отсутствие ожидаемых ключей в ответе API.')
+    verdict = HOMEWORK_VERDICTS[status]
+    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+
+
+def send_message(bot, message):
+    """Отправляет в telegram сообщение."""
+    try:
+        bot.sendMessage(TELEGRAM_CHAT_ID, message)
+        logging.debug('Сообщение о смене статуса отправлено.')
+    except Exception:
+        logging.error('Ошибка в отправке сообщения.')
 
 
 def main():
     """Основная логика работы бота."""
     bot = Bot(token=TELEGRAM_TOKEN)
-    timestamp = int(time.time()) - 600
-    answer = get_api_answer(timestamp)
-    message = parse_status(answer)
-    if message:
+    timestamp = int(time.time()) - 600000
+    try:
+        # Проверяет доступность переменных окружения.
+        check_tokens()
+        # Сохраняем ответ от эндпоинта.
+        answer = get_api_answer(timestamp)
+        # Проверяем ответ на соответствие документации.
+        check_response(answer)
+        # Формируем сообщение.
+        message = parse_status(answer)
+        # Отправляем сообщение.
         send_message(bot, message)
-
-    while True:
-        try:
-            # Запуск функции каждые 10 минут
-            schedule.every(10).minutes.do(main)
-            # Бесконечный цикл для выполнения расписания
-            while True:
-                schedule.run_pending()
-                time.sleep(1)
-
-        except Exception as error:
-            message = f'Сбой в работе программы: {error}'
-            ...
-        ...
+    except Exception as error:
+        message = f'Сбой в работе программы: {error}'
+        # Отправляем сообщение.
+        send_message(bot, message)
 
 
 if __name__ == '__main__':
-    main()
+    # Запуск функции каждые 10 минут
+    schedule.every(1).minutes.do(main)
+    # Бесконечный цикл для выполнения расписания
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
